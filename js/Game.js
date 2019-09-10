fd3f2cf30a6eff665185ed9de728ff92b68230f1
Ja_Lew <dladyj@gmail.com>
2019-09-10 16:26:08 -0400

    Added Project 4 Files

---
+++ js/Game.js
@@ -0,0 +1,19 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game {
+     constructor(){
+        missed('0');
+        phrases();
+        activePhrases('null');
+        startGame(){
+            getRandomPhrase(){
+                
+            }
+        }
+
+         
+
+     }
+ }
---
+++ js/Phrase.js
@@ -0,0 +1,20 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js 
+ * 
+ * Jane Lewis*/
+
+class Phrase {
+    constructor(phrase){
+    }
+        addPhraseToDisplay(){
+
+        };
+        checkLetter(){
+
+        };
+        showMatchedLetter(){
+
+        };  
+    
+};

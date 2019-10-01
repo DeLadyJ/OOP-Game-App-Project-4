562c85258a5563269c7e45246358116ed31b32c9
Ja_Lew <dladyj@gmail.com>
2019-10-01 14:52:31 -0400

    Added Phrases to Game.js

--- js/Game.js
+++ js/Game.js
@@ -4,10 +4,21 @@
 
  class Game {
      constructor(){
-        missed();
-        phrases();
-        activePhrases('null');
-        startGame();
-            getRandomPhrase();
+        this.misses = 0;
+        this.activePhrase = null;
+        this.phrases = [
+          new phrase("when they go low we go high"),
+          new phrase("i wish my wallet came with refills"),
+          new phrase("three little birds"),
+          new phrase("those who do not listen shall feel"),
+          new phrase("") ];
+       }
+       /**
+    * Gets random phrase from phrases, then returns the randow phrase
+    */
+       getRandomPhrase(){
+         this.currentPhrase = this.phrase[Math.floor(Math.random() * phrases.length)];
+           return randomPhrase;
+         }
  }
 }

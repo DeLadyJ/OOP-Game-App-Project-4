7fb3359aa8aaa5aed02c948519e7e8cf729241ad
Ja_Lew <dladyj@gmail.com>
2019-10-02 12:08:00 -0400

    Added Placeholder for Letter and Spaces
    also added the start game function

--- js/Game.js
+++ js/Game.js
@@ -11,7 +11,8 @@
           new phrase("i wish my wallet came with refills"),
           new phrase("three little birds"),
           new phrase("those who do not listen shall feel"),
-          new phrase("") ];
+          new phrase("")
+        ];
        }
        /**
     * Gets random phrase from phrases, then returns the randow phrase
@@ -20,5 +21,8 @@
          this.currentPhrase = this.phrase[Math.floor(Math.random() * phrases.length)];
            return randomPhrase;
          }
+         startGame(){
+          //hide the element with the id of overlay
+          document.getElementById('overlay').style.display = 'none';
+          }
  }
-}
--- js/Phrase.js
+++ js/Phrase.js
@@ -8,13 +8,27 @@ class Phrase {
     constructor(phrase){
     }
         addPhraseToDisplay(){
-    //loops through phrase to check for letter or space and adds a letter or space class
+    
+    //Get phrase then displays on screen
     for (let i = 0; i < this.phrase.length; i++){
         this.phrase[i] = "";
    //const placeholder = document.createElement(li);
+   //loops through phrase to check for letter or space and adds a letter or space class
+           //loops through phrase to check for letter or space and adds a letter or space class
+       //const placeholder = document.createElement(li);
+       //let li = document.createElement('li');
+           //let letter = (this.phrase[1];  'hide letter ${this.phrase[1]}';
+           //placeHolder.appendChild(placeHolder);
+           if ( letter === "") {
+             li.className = 'hide space'; //gives the space class if there is a li with a space
+           } else {
+             li.className = 'hide letter ${letter}' ; //gives the letter class if li has a letter
+           }
+           li.textContent = 'letter';
+           phraseDiv.appendChild(li);
     }
   }
-        };
+        
         checkLetter(){
 
         };

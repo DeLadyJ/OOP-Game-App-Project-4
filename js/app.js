51c9bdd7e8007bf6219833ab4192764a8f936171
Ja_Lew <dladyj@gmail.com>
2019-10-22 09:07:21 -0400

    Added Event Listeners
    added eventListeners for reset button, qwerty

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@
 
  let game;
 
- 
+ document.querySelector('#btn__reset').addEventListener('click', function() {
+    game = new Game ();
+    game.startGame();
+
+    });
+    
+    document.querySelector('#qwerty').addEventListener('click', e => {    // When any letter is clicked, handleInteractions will be called
+       if(e.target.className === 'key') {
+           game.handleInteraction(e.target);
+       }
+    })

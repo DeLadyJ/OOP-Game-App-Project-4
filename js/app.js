7d05960a0835466f99a7ff4c6aaf7eac70c8f75b
Ja_Lew <dladyj@gmail.com>
2019-10-23 18:59:54 -0400

    changed the addEventLister in App.js
    updated the game.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+//const qwerty = document.querySelector('#qwerty');
 
- //let game;
-
+ let game;
  document.querySelector('#btn__reset').addEventListener('click', function() {
     game = new Game ();
     game.startGame();
+ });
+ //.addEventListener('click', function() {
+ //document.querySelector('#startGame').addEventListener('click', function() {
+  
 
-    });
+
     
-    document.querySelector('#qwerty').addEventListener('click', e => {    // When any letter is clicked, handleInteractions will be called
-       if(e.target.className === 'key') {
-           game.handleInteraction(e.target);
-       }
-    })
+    // document.querySelector('#qwerty').addEventListener('click', e => {    // When any letter is clicked, handleInteractions will be called
+    //    if(e.target.className === 'key') {
+    //        game.handleInteraction(e.target);
+    //    }
+    // })
